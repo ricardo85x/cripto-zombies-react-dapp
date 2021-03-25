0c89a5c3108657553356edeae6dab807923de577
Ricardo <ricardoinliner@yahoo.com>
2021-03-24 21:46:51 -0300

    Display simple list of zombies from smart contract

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Web3 from 'web3';
 import Web3Modal from "web3modal";
 
 import { ZombieContracts as ZombieContractsRaw } from "./solidity/config"
 
-interface ZombieContractsInterface {
+interface ZombieContractsProps {
   ZombieOwnership: {
     address: string,
     abi: any
   }
 }
 
-const ZombieContracts: ZombieContractsInterface = ZombieContractsRaw
+const ZombieContracts: ZombieContractsProps = ZombieContractsRaw
 
-interface ZombieInterface {
+interface ZombieProps {
   dna: number,
   name: string,
   level: number,
@@ -22,25 +22,26 @@ interface ZombieInterface {
   lossCount: number
 }
 
-interface ZombieFactoryContractInterface {
+interface ZombieFactoryContractProps {
   methods: {
     createRandomZombie: Function,
     ownerZombieCount: Function,
-    zombies: (id: number) => { call: () => ZombieInterface }
+    zombies: (id: number) => { call: () => ZombieProps },
+    zombieToOwner : (id: number) => { call: () => string },
+    getZombiesByOwner : (owner: string) => { call: () => Array<number> },
+    
   },
 }
 
 function App() {
 
   const [accountAddress, setAccountAddress] = useState<string>()
-  const [zombieFactory, setZombieFactory] = useState<ZombieFactoryContractInterface>()
+  const [zombieFactory, setZombieFactory] = useState<ZombieFactoryContractProps>()
   const [zombie_name, setZombieName] = useState<string>("NoName")
   const [nZombies, setNZombies] = useState(0)
+  const [zombieArmy, setZombieArmy] = useState<ZombieProps[]>([])
   const [web3Enabled, setWeb3Enabled] = useState(false)
 
-  useEffect(() => {
-
-  },[])
 
   const connectWeb3 = async () => {
 
@@ -75,18 +76,71 @@ function App() {
         setNZombies(_nZombies)
       }
 
-      setAccountAddress(_accounts[0])
       setZombieFactory(_zombieFactory)
+      setAccountAddress(_accounts[0])
       setWeb3Enabled(true)
+
+      provider.on("accountsChanged", (accounts: string[]) => {
+        console.log("accountsChanged", accounts);
+        setAccountAddress(accounts[0])
+      });
+      
+      // Subscribe to chainId change
+      provider.on("chainChanged", (chainId: number) => {
+        console.log("chainChanged", chainId);
+      });
+      
+      // Subscribe to provider connection
+      provider.on("connect", (info: { chainId: number }) => {
+        console.log("connect", info);
+      });
+      
+      // Subscribe to provider disconnection
+      provider.on("disconnect", (error: { code: number; message: string }) => {
+        console.log("disconnect", error);
+      });
+
     }
   }
 
-  const getZombiesDatails = (id: number) => {
+  useEffect(() => {
+    displayZombies()
+  }, [accountAddress])
+
+  const displayZombies = async () => {
+    if (accountAddress){
+      const ids = await getZombiesByOwner(accountAddress);
+      if (ids) {
+        const army :Array<ZombieProps> = []
+        for ( const id of ids ) {
+          const zombie = await getZombiesDatails(id);
+          if (zombie) {
+            army.push(zombie)
+          }
+        }
+        setZombieArmy(army);
+      }
+    }
+  }
+
+
+  const getZombiesDatails =  (id: number) => {
     return zombieFactory?.methods.zombies(id).call()
   }
 
-  const createZombie = async () => {
+  const zombieToOwner = (id: number) => {
+    return zombieFactory?.methods.zombieToOwner(id).call()
+  }
 
+  const getZombiesByOwner = (owner: string) => {
+    return zombieFactory?.methods.getZombiesByOwner(owner).call()
+  }
+
+  const ownerZombieCount = async (accountAddress: string) => {
+    return zombieFactory?.methods.ownerZombieCount(accountAddress).call()
+  }
+
+  const createZombie = async () => {
     if (nZombies == 0) {
       zombieFactory?.methods.createRandomZombie(zombie_name).send( { 
         from: accountAddress,
@@ -94,34 +148,40 @@ function App() {
         gasPrice: 20000000000
       }).once('receipt', (receipt: any) => {
         console.log(receipt)
+        const { status } = receipt
+        if ( status ){
+          setNZombies(1)
+          displayZombies()
+        }
       })
     }
-
-  }
-
-
-  const showFactory = async () => {
-    console.log(zombieFactory)
-
-    const nZombies = await zombieFactory?.methods.ownerZombieCount(accountAddress).call()
-    console.log(nZombies)
   }
 
   return (
     <div className="App">
       <h1>Hello Reacto Zombies</h1>
-
       {
         !web3Enabled && <button onClick={connectWeb3}>Connect</button>
       }
-
       {
         web3Enabled && (
           <>
-            <h3>Address: {accountAddress}</h3>
+            <h3>My Address: {accountAddress}</h3>
 
-            <button onClick={() => showFactory()}>Debug Factory</button>
-            
+            <div>
+            { zombieArmy.map( zombie => (
+                <ul key={zombie.dna}>
+                  <li>Name: {zombie.name}</li>
+                  <li>DNA: {zombie.dna}</li>
+                  <li>Level: {zombie.level}</li>
+                  <li>Wins: {zombie.winCount}</li>
+                  <li>Losses: {zombie.lossCount}</li>
+                  <li>Ready Time: {zombie.readyTime}</li>
+                </ul>
+
+            ) )}
+            </div>
+
             { nZombies == 0 && ( 
               <div>
                 <input type="text" onChange={(el) => setZombieName(el.target.value)} />
